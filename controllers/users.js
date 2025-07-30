1ad0089e1e83da4872f8b1c511ea282271a910ee
mignrod <mignrod@gmail.com>
2025-07-30 16:15:13 -0400

    fixing

--- controllers/users.js
+++ controllers/users.js
@@ -50,23 +50,23 @@ const updateUser = async (req, res) => {
   try {
     const { id } = req.params;
     const { name, email, password, nickname, age } = req.body;
-    const user = await User.findByIdAndUpdate(
-      id,
-      {
-        name,
-        email,
-        password,
-        nickname,
-        age
-      },
-      { new: true }
-    );
+    const updateFields = {};
+    if (name !== undefined) updateFields.name = name;
+    if (email !== undefined) updateFields.email = email;
+    if (password !== undefined) updateFields.password = password;
+    if (nickname !== undefined) updateFields.nickname = nickname;
+    if (age !== undefined) updateFields.age = age;
+
+    const user = await User.findByIdAndUpdate(id, updateFields, {
+      new: true,
+      runValidators: true
+    });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 };
 

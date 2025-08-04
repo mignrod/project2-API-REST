979a50bd2603cc051224e39bf53581bfa6cd0ca5
mignrod <mignrod@gmail.com>
2025-08-03 20:51:15 -0400

    production

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.use(
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL: process.env.CALLBACK_URL
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       const userData = {
         profile,
         accessToken
